18341d99aaa7e6d5a1388c800ef2a93b34e1e62d
itsmontoya <josh@meteora.co>
2015-03-05 20:30:59 -0800

    Some build fixes

--- main.go
+++ main.go
@@ -1,21 +1,17 @@
 package outputBandit
 
 import (
-	"bufio"
-	"fmt"
 	"os"
 	"sync"
 )
 
-func NewBandit(outLoc, errLoc string) *OutputBandit {
-	outF, _ := os.Create(outLoc)
-	errF, _ := os.Create(errLoc)
+func New(outLoc, errLoc string) *OutputBandit {
+	out, _ := os.Create(outLoc)
+	err, _ := os.Create(errLoc)
 
 	o := OutputBandit{
-		out:        bufio.NewWriter(outF),
-		err:        bufio.NewWriter(errF),
-		outF:       outF,
-		errF:       errF,
+		out:        out,
+		err:        err,
 		origStdOut: os.Stdout,
 		origStdErr: os.Stderr,
 	}
@@ -29,25 +25,19 @@ func NewBandit(outLoc, errLoc string) *OutputBandit {
 type OutputBandit struct {
 	sync.RWMutex
 
-	out *bufio.Writer
-	err *bufio.Writer
+	out *os.File
+	err *os.File
 
-	outF *os.File
-	errF *os.File
-
-	origStdOut *os.Stderr
-	origStdErr *os.Stdout
+	origStdOut *os.File
+	origStdErr *os.File
 }
 
 func (o *OutputBandit) Close() {
 	o.Lock()
 	defer o.Unlock()
 
-	o.out.Flush()
-	o.err.Flush()
-
-	o.outF.Close()
-	o.errF.Close()
+	o.out.Close()
+	o.err.Close()
 
 	os.Stdout = o.origStdOut
 	os.Stderr = o.origStdErr
