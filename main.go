5a5ba0a87198b1ae47661af9363e54491ef0e900
itsmontoya <josh@meteora.co>
2015-03-05 21:10:20 -0800

    Changed order of executed commands within the Close function. Added comments where necessary.

--- main.go
+++ main.go
@@ -5,6 +5,10 @@ import (
 	"os"
 )
 
+// Creates and returns a new instance of Output Bandit:
+// 		- Opens both an output and error file based on the locations provided
+// 		- Sets Stdout to the output file and Stderr to the error file
+// 		- Sets the output for the log package to the new Stderr
 func New(outLoc, errLoc string) *OutputBandit {
 	out, _ := os.Create(outLoc)
 	err, _ := os.Create(errLoc)
@@ -32,11 +36,15 @@ type OutputBandit struct {
 	errOrig *os.File
 }
 
+// Closes Output Bandit:
+// 		- Sets global Stdout and Stderr back to the original
+// 		- Sets the output for the log package to the original Stderr
+// 		- Closes the output and error file
 func (o *OutputBandit) Close() {
-	o.out.Close()
-	o.err.Close()
-
 	os.Stdout = o.outOrig
 	os.Stderr = o.errOrig
 	log.SetOutput(o.errOrig)
+
+	o.out.Close()
+	o.err.Close()
 }
