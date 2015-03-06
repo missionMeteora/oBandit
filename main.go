72cf96ef501f4609e65a3352de27f05ec8d90d4d
itsmontoya <josh@meteora.co>
2015-03-05 20:41:58 -0800

    Cleaning up

--- main.go
+++ main.go
@@ -9,15 +9,13 @@ func New(outLoc, errLoc string) *OutputBandit {
 	out, _ := os.Create(outLoc)
 	err, _ := os.Create(errLoc)
 
-	o := OutputBandit{
-		out:        out,
-		err:        err,
-		origStdOut: os.Stdout,
-		origStdErr: os.Stderr,
-	}
+	os.Stdout = out
+	os.Stderr = err
 
-	os.Stdout = o.out
-	os.Stderr = o.err
+	o := OutputBandit{
+		out: out,
+		err: err,
+	}
 
 	return &o
 }
@@ -27,18 +25,9 @@ type OutputBandit struct {
 
 	out *os.File
 	err *os.File
-
-	origStdOut *os.File
-	origStdErr *os.File
 }
 
 func (o *OutputBandit) Close() {
-	o.Lock()
-	defer o.Unlock()
-
 	o.out.Close()
 	o.err.Close()
-
-	os.Stdout = o.origStdOut
-	os.Stderr = o.origStdErr
 }
