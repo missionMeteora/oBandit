5b5ef461db3392a55608ab441a09a373282758ea
itsmontoya <josh@meteora.co>
2015-03-10 12:52:28 -0700

    Added in low level goodness

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package outputBandit
 
 import (
-	"log"
 	"os"
 	"syscall"
+)
+
+const (
+	outTmpLoc = ".bndtOut"
+	errTmpLoc = ".bndtErr"
 )
 
 // Creates and returns a new instance of Output Bandit:
@@ -11,38 +15,88 @@ import (
 // 		- Sets Stdout to the output file and Stderr to the error file
 // 		- Sets the output for the log package to the new Stderr
 func New(outLoc, errLoc string) (*OutputBandit, error) {
-	outF, err := os.Create(outLoc)
-	if err != nil {
-		return nil, err
-	}
-
-	errF, err := os.Create(errLoc)
-	if err != nil {
-		return nil, err
-	}
-
 	o := OutputBandit{
-		out: outF,
-		err: errF,
-
-		outOrig: os.Stdout,
-		errOrig: os.Stderr,
+		outOrig:   os.Stdout,
+		errOrig:   os.Stderr,
+		outOrigFd: int(os.Stdout.Fd()),
+		errOrigFd: int(os.Stderr.Fd()),
 	}
 
-	os.Stdout = outF
-	os.Stderr = errF
-	log.SetOutput(errF)
-	syscall.Dup2(int(errF.Fd()), 2)
+	o.setFiles(outLoc, errLoc)
+	o.setTmpFiles()
+	o.hijack()
 
 	return &o, nil
 }
 
 type OutputBandit struct {
-	out *os.File
-	err *os.File
+	out   *os.File
+	err   *os.File
+	outFd int
+	errFd int
 
-	outOrig *os.File
-	errOrig *os.File
+	outOrig   *os.File
+	errOrig   *os.File
+	outOrigFd int
+	errOrigFd int
+
+	outTmp   *os.File
+	errTmp   *os.File
+	outTmpFd int
+	errTmpFd int
+}
+
+func (o *OutputBandit) setFiles(outLoc, errLoc string) error {
+	outF, err := os.Create(outLoc)
+	if err != nil {
+		return err
+	}
+
+	errF, err := os.Create(errLoc)
+	if err != nil {
+		return err
+	}
+
+	o.out = outF
+	o.err = errF
+
+	o.outFd = int(outF.Fd())
+	o.errFd = int(errF.Fd())
+
+	return nil
+}
+
+func (o *OutputBandit) setTmpFiles() error {
+	outTmpF, err := os.Create(outTmpLoc)
+	if err != nil {
+		return err
+	}
+
+	errTmpF, err := os.Create(errTmpLoc)
+	if err != nil {
+		return err
+	}
+
+	o.outTmp = outTmpF
+	o.errTmp = errTmpF
+
+	return nil
+}
+
+func (o *OutputBandit) hijack() {
+	syscall.Dup2(o.outOrigFd, o.outTmpFd)
+	syscall.Dup2(o.outFd, o.outOrigFd)
+
+	syscall.Dup2(o.errOrigFd, o.errTmpFd)
+	syscall.Dup2(o.errFd, o.errOrigFd)
+}
+
+func (o *OutputBandit) unhijack() {
+	syscall.Dup2(o.outOrigFd, o.outFd)
+	syscall.Dup2(o.outTmpFd, o.outOrigFd)
+
+	syscall.Dup2(o.errOrigFd, o.errFd)
+	syscall.Dup2(o.errTmpFd, o.errOrigFd)
 }
 
 // Closes Output Bandit:
@@ -50,10 +104,12 @@ type OutputBandit struct {
 // 		- Sets the output for the log package to the original Stderr
 // 		- Closes the output and error file
 func (o *OutputBandit) Close() {
-	os.Stdout = o.outOrig
-	os.Stderr = o.errOrig
-	log.SetOutput(o.errOrig)
-
+	o.unhijack()
 	o.out.Close()
 	o.err.Close()
+	o.outTmp.Close()
+	o.errTmp.Close()
+
+	os.Remove(outTmpLoc)
+	os.Remove(errTmpLoc)
 }
